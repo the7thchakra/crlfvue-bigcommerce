2f3ab2d9ccfc2120b99941f70bab66e1784c172f
Stephanie Victoria Toupin <stephanie@toupin.com>
2021-02-26 11:02:19 -0800

    implemented requested features

--- assets/js/theme/category.js
+++ assets/js/theme/category.js
@@ -32,6 +32,10 @@ export default class Category extends CatalogPage {
 
         $('[data-button-type="add-cart"]').on('click', (e) => this.setLiveRegionAttributes($(e.currentTarget).next(), 'status', 'polite'));
 
+        this.setUpImageRollover();
+        this.setUpAddAllToCart();
+        this.setUpRemoveAllFromCart();
+
         this.makeShopByPriceFilterAccessible();
 
         compareProducts(this.context.urls);
@@ -46,6 +50,102 @@ export default class Category extends CatalogPage {
         $('a.reset-btn').on('click', () => this.setLiveRegionsAttributes($('span.reset-message'), 'status', 'polite'));
 
         this.ariaNotifyNoProducts();
+    }
+
+    setUpImageRollover() {
+        this.saveOriginalImage();
+        $('figure').on('mouseenter', (e) => this.showAlternateImage(e));
+        $('figure').on('mouseleave', (e) => this.showOriginalImage(e));
+    }
+
+    saveOriginalImage() {
+        $('figure img[rollover_src]').each((i, img) => {
+            var $img = $(img);
+            $img
+                .attr('data-original_srcset', $img.attr('data-srcset'))
+                .attr('original_srcset', $img.attr('srcset'))
+                .attr('original_src', $img.attr('src'))
+                .attr('original_alt', $img.attr('alt'))
+        });
+    }
+
+    showAlternateImage(e) {
+        var $img = $(e.target).find('img[rollover_src]');
+        $img
+            .attr('data-srcset', '')
+            .attr('srcset', '')
+            .attr('src', $img.attr('rollover_src'))
+            .attr('alt', $img.attr('rollover_alt'));
+    }
+
+    showOriginalImage(e) {
+        var $img = $(e.target).find('img[rollover_src]');
+        $img
+            .attr('data-srcset', $img.attr('data-original_srcset'))
+            .attr('srcset', $img.attr('original_srcset'))
+            .attr('src', $img.attr('original_src'))
+            .attr('alt', $img.attr('original_alt'));
+    }
+
+    setUpAddAllToCart() {
+        var self = this;
+        $('[data-button-type="add-all-to-cart"]').on('click', (e) => {
+            var productIds = self.getAllProductIds();
+            debugger;
+            fetch('/api/storefront/carts/' + self.context.cartId, {
+                method: "POST",
+                credentials: "same-origin",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(self.buildAddLineItemsRequest(productIds)),
+            })
+            .then(response => {
+                alert($('[data-all-items-added]').text());
+            })
+        });
+    }
+
+    buildAddLineItemsRequest(productIds) {
+        return {
+            lineItems: productIds.map((productId) => {
+                return {
+                    productId: productId,
+                    quantity: "1",
+                };
+            }),
+        };
+    }
+
+    getAllProductIds() {
+        var ids = [ ];
+        var seen = { };
+        $('[data-product-id]').each((index, element) => {
+            var productId = $(element).attr('data-product-id');
+            if (!seen[productId]) {
+                ids.push(productId);
+                seen[productId] = true;
+            }
+        });
+        return ids;
+    }
+
+    setUpRemoveAllFromCart() {
+        var self = this;
+        $('[data-button-type="remove-all-from-cart"]').on('click', (e) => {
+            fetch('/api/storefront/carts/' + self.context.cartId, {
+                method: "DELETE",
+                credentials: "same-origin",
+                headers: {
+                    "Content-Type": "application/json",
+                }
+            })
+            .then(response => {
+                $('[data-button-type="remove-all-from-cart"]').parent().hide();
+                
+                alert($('[data-all-items-removed]').text());
+            })
+        });
     }
 
     ariaNotifyNoProducts() {
